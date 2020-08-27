4dfd86d4b0dd0253bc4d1e0f1f47f9692bcf65b6
Resy31 <resy3311yahoo.co.jp>
2020-08-27 19:39:09 +0900

    active_hashを用いたコントローラーの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
-  def index
+  def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.seve
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :describe, :price, :category_id, :delivery_fee_id, :ship_address_id, :ship_date_id, :status_id)
+  end
+
 end
