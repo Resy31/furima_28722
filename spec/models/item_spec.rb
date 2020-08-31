30ac156641e2d09547a4f80a4903e53e088bb0c0
Resy31 <resy3311yahoo.co.jp>
2020-08-31 21:58:16 +0900

    テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -60,8 +60,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格の範囲が、¥300~¥9,999,999の間であること' do
+      it '価格が300円未満のとき' do
         @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+      it '価格が10,000,000円以上のとき' do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
