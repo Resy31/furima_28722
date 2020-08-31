d87bcddf9adef0031be80cc7cd5b394ff60b719c
Resy31 <resy3311yahoo.co.jp>
2020-08-31 15:32:44 +0900

    販売手数料の計算機能

---
+++ app/javascript/tax.js
@@ -0,0 +1,18 @@
+function tax() {
+  const price_tax = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const price_profit = document.getElementById("profit");
+  price_tax.addEventListener('input', function(){
+    const price = this.value;
+    if(300 <= price && price <= 9999999) {
+    const fee = Math.floor(price / 10);
+    const profit = (price - fee);
+    tax.innerHTML = fee;
+    price_profit.innerHTML = profit;
+    }else{
+    tax.innerHTML = '';
+    price_profit.innerHTML = '';
+    }
+  })
+};
+window.addEventListener('load', tax);
