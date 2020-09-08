67c9411ab574da672feed923988357a631327c91
Resy31 <resy3311yahoo.co.jp>
2020-09-08 16:46:27 +0900

    トークンの作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,12 +7,33 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = purchase.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def move_to_session
     redirect_to user_session_path unless user_signed_in?
+  end
+
+  def purchase_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_97f723bd57d8b814eca0e770"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: item_params[:price],    # 商品の値段
+      card: purchase_params[:token],  # カードトークン
+      currency:'jpy'                  # 通貨の種類(日本円)
+    )
   end
 
 end
