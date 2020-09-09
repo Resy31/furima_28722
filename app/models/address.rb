ab33ecbaa9f4f65d57cda48618e04207a55779c9
Resy31 <resy3311yahoo.co.jp>
2020-09-09 19:59:54 +0900

    formオブジェクトに移動

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,4 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :ship_address
-
-  validates :zip_code,     presence: true
-  validates :city,         presence: true
-  validates :house_number, presence: true
-  validates :phone_number, presence: true
-  validates :ship_address, presence: true
-
-  validates :ship_address_id { other_than: 1, message: ' Select' }
-
+  belongs_to :purchase
 end
