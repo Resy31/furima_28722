b3db0601685a80f71c533744748b9b1d5d0c98d7
Resy31 <resy3311yahoo.co.jp>
2020-09-11 16:05:10 +0900

    コントローラーの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -29,15 +29,10 @@ class PurchasesController < ApplicationController
     params.require(:item_purchase).permit(:zip_code, :ship_address_id, :city, :house_number, :apartment, :phone_number, :token).merge( item_id: params[:item_id], user_id: current_user.id )
   end
 
-  def set_purchase
-    @purchase = Item.find(purchase_params[:item_id])
-  end
-
-
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: set_purchase[:price],    # 商品の値段
+      amount: @item.price,    # 商品の値段
       card: purchase_params[:token],  # カードトークン
       currency:'jpy'                  # 通貨の種類(日本円)
     )
