cff4a6eb1cc0b38a82286888bf6f7d893fb17575
Resy31 <resy3311yahoo.co.jp>
2020-09-11 11:40:58 +0900

    nameをformオブジェクトと統一

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_195627ccf4ab294cce8cff2f"); // PAY.JPテスト公開鍵
+  const Payjp_pk = process.env.PAYJP_PUBLIC_KEY
+  Payjp.setPublicKey(Payjp_pk);  // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");//charge-form内のsubmitの実行
   form.addEventListener("submit", (e) => {
     e.preventDefault(); //railsではなく、JSでサーバーサイドに送るためにフォームの送信キャンセル
@@ -8,16 +9,16 @@ const pay = () => {
     const formData = new FormData(formResult);
  
     const card = {
-      number: formData.get("number"),
-      cvc: formData.get("cvc"),
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`, //カード情報を変数に
+      number: formData.get("item_purchase[number]"),
+      cvc: formData.get("item_purchase[cvc]"),
+      exp_month: formData.get("item_purchase[exp_month]"),
+      exp_year: `20${formData.get("item_purchase[exp_year]")}`, //カード情報を変数に
     };
     Payjp.createToken(card, (status, response) => { //PAY.JPにアクセス
       if (status == 200) { //ステータス200でトークンの生成が成功であればif内のコードを実行
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} type="hidden" name='token'>`;//コントローラーに受け渡しているname
+        const tokenObj = `<input value=${token} type="hidden" name='item_purchase[token]'>`;//コントローラーに受け渡しているname
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
       document.getElementById("card-number").removeAttribute("name");
