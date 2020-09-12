2ad28b8d0431f0ebed97ec92f18f796dbc02b612
Resy31 <resy3311yahoo.co.jp>
2020-09-12 17:17:39 +0900

    コントローラー の修正

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -2,21 +2,18 @@ class ItemPurchase
   include ActiveModel::Model
   attr_accessor :zip_code, :ship_address_id, :city, :house_number, :apartment, :phone_number, :user_id, :item_id, :token
 
-  validates :token,        presence: true
-  validates :zip_code,     presence: true
-  validates :city,         presence: true
-  validates :house_number, presence: true
   validates :ship_address_id, numericality: { other_than: 1, message: 'Select' }
 
   with_options presence: true do
     validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/ }
+    validates :token, :zip_code, :city, :house_number
   end
 
   ZIP_CODE_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
   validates_format_of :zip_code, with: ZIP_CODE_REGEX, message: 'Input correctly'
 
   def save
-    item_purchase = Purchase.create!(user_id: user_id, item_id: item_id)
-    Address.create!(zip_code: zip_code, ship_address_id: ship_address_id, city: city, house_number: house_number, apartment: apartment, phone_number: phone_number, purchase_id: item_purchase.id)
+    item_purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(zip_code: zip_code, ship_address_id: ship_address_id, city: city, house_number: house_number, apartment: apartment, phone_number: phone_number, purchase_id: item_purchase.id)
   end
 end
